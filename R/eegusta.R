facf8ea437c39c38b5a332ed568aea74aaa3dc92
livio <livio.finos@unipd.it>
2022-05-03 23:46:29 +0200

    ...

--- R/eegusta.R
+++ R/eegusta.R
@@ -1,3 +1,5 @@
-#' @keywords internal
-#' @author Livio Finos, Angela Andreella
+#' @keywords eeg
+#' @author Livio Finos, Antonio Maffei, Angela Andreella
+#' @title  EEG Utilities and STatistical Analysis: eeguana-based tools for statistical analysis of EEG data 
+#' @description EEG Utilities and STatistical Analysis: eeguana-based tools for statistical analysis of eeg data. It contains a few utiliy functions and some examples of import, preprocessing and Statistical Analysis of EEG data (see Vignettes folder). This project is a work in progress.
 "_PACKAGE"
